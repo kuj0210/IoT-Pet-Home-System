133c812877ea7971fb4fa5f5af44403e899deef0
KeonHee Lee <31424664+KeonHeeLee@users.noreply.github.com>
2017-12-30 19:17:18 +0900

    v0.5 파이서버와 통신

---
+++ PiServer.py
@@ -0,0 +1,44 @@
+from flask import Flask,request,jsonify
+
+app = Flask(__name__)
+
+@app.route("/feed",methods=["POST"])
+def setFeed():
+    message = {
+        "message" : {
+            "text" : "펫에게 먹이를 주고 있어요."
+        }
+    }
+    return jsonify(message), 200
+
+@app.route("/water", methods=["POST"])
+def setWater():
+    message = {
+        "message" : {
+            "text" : "펫에게 물을 주고 있어요."
+        }
+    }
+    return jsonify(message), 200
+
+
+@app.route("/door", methods=["POST"])
+def setDoor():
+    message = {
+        "message" : {
+            "text" : "펫 하우스를 개방합니다."
+        }
+    }
+    return jsonify(message), 200
+
+
+@app.route("/camera", methods=["POST"])
+def captureCamera():
+    message = {
+        "message" : {
+            "text" : "사진을 찍었어요."
+        }
+    }
+    return jsonify(message), 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8888, debug = True)
