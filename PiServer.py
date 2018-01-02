801cb2ae07e197ef4af8e44727de56a765f2a1d1
KeonHee Lee <31424664+KeonHeeLee@users.noreply.github.com>
2018-01-03 07:49:39 +0900

    v0.6 connect Pi

--- PiServer.py
+++ PiServer.py
@@ -1,45 +1,66 @@
-from flask import Flask,request,jsonify
+from flask import Flask,jsonify
+from PiMessage import PiMessage
+from WaterOperation import WaterOperation
+#import picamera
+
+message = PiMessage()
+
+flag = {
+    "isWater": False,
+    "isFeed": False,
+    "isDoor": False
+}
 
 app = Flask(__name__)
 
 @app.route("/feed",methods=["POST"])
 def setFeed():
-    message = {
-        "message" : {
-            "text" : "펫에게 먹이를 주고 있어요."
-        }
-    }
-    return jsonify(message), 200
+    #
+    # It it requeired to input Threadig function.
+    # Becaues this operation must execve parrelling behaviour.
+    # :: This part is "/feed". Also, it get FeedOperaion class.
+    #
+    return jsonify(message.getSuccessFeedMessage()), 200
 
 @app.route("/water", methods=["POST"])
 def setWater():
-    message = {
-        "message" : {
-            "text" : "펫에게 물을 주고 있어요."
-        }
-    }
-    return jsonify(message), 200
+    if flag["isWater"] == False:
+        flag["isWater"] = True
+        water = WaterOperation()
+        result = water.start()
+        #with picamera.PiCamera() as camera:
+        #    now = time.localtime()
+        #    camera.start_preview()
+        #    time.sleep(1)
+        #    path = '/home/pi/Pictures/Record %04d-%02d-%02d %02d:%02d:%02d.png' % (
+        #    now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+        #    camera.capture(path)
+        #    camera.stop_preview()
 
+        flag["isWater"] = False
+
+        if result is "success" :
+            return jsonify(message.getSuccessWaterMessage()), 200
+        elif result is "fail" :
+            return jsonify(message.getFailWaterMessage()), 200
+
+    else : # if flag["isWater"] == True:
+        return jsonify(message.getContinueWaterMessage()), 200
 
 @app.route("/door", methods=["POST"])
 def setDoor():
-    message = {
-        "message" : {
-            "text" : "펫 하우스를 개방합니다."
-        }
-    }
-    return jsonify(message), 200
+    #
+    # It it requeired to input Threadig function.
+    # Becaues this operation must execve parrelling behaviour.
+    # :: This part is "/door". Also, it get doorOperaion class.
+    #
+    return jsonify(message.getSuccessDoorMessage()), 200
 
 
 @app.route("/camera", methods=["POST"])
 def captureCamera():
-    message = {
-        "message" : {
-            "text" : "사진을 찍었어요."
-        }
-    }
-    return jsonify(message), 200
+
+    return jsonify(message.getSuccessCameraMessage()), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8888, debug = True)
-    
---
+++ Server.py
@@ -0,0 +1,44 @@
+#-*-coding: utf-8-*-
+
+from flask import Flask,request,jsonify
+from JsonMessage import MessageClass
+
+
+## json 타입들 초기화
+
+mMessage = MessageClass()
+app = Flask(__name__)
+## Flask 키는 소스
+
+@app.route("/keyboard",methods =["GET"])
+def keyboard():
+    return jsonify(mMessage.getBaseKeyboard()),200
+
+## 키보드 정보 가져옴
+
+@app.route("/message",methods=["POST"])
+def message():
+    data = request.get_json()   ## 요청메세지로부터 json 데이터를 가져옴
+                                ## 게다가 딕셔너리 형태로 반환해줌
+    message = data['content']
+    return jsonify(mMessage.postTextMessage(message=message)),200
+## 메세지를 보냈을 때 처리
+
+@app.route("/friend",methods=["POST"])
+def friendAdd():
+    message = request.get_json()
+    return "HTTP/1.1 200 OK"
+## 친구 추가했을 때
+
+@app.route("/friend/<user_key>", methods=["DELETE"])
+def friendDelete(user_key):
+    return "HTTP/1.1 200 OK"
+## 친구 삭제했을 때
+
+@app.route("/chat_room/<user_key>",methods=["DELETE"])
+def chat_roomOut(user_key):
+    return "HTTP/1.1 200 OK"
+## 채팅방에서 나갔을 때
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug = True)
