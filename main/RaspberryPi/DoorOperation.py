341bf0658e5cebdc75a0fccecba5303c77009045
KeonHee Lee <31424664+KeonHeeLee@users.noreply.github.com>
2018-01-15 20:23:31 +0900

    Update DoorOperation.py

--- main/RaspberryPi/DoorOperation.py
+++ main/RaspberryPi/DoorOperation.py
@@ -5,14 +5,21 @@ import threading
 class DoorOperation(threading.Thread):
     def __init__(self):
         self.motor = 0
-        self.TERM = 8
+        self.OPEN_TERM = 10
+        self.STOP_TERM = 3
+        self.CLOSE_TERM = 4
         threading.Thread.__init__(self)
 
     def run(self):
         self.motor.start(0)
         try:
-            self.motor.ChangeDutyCycle(18)
-            time.sleep(self.TERM)
+            self.motor.ChangeDutyCycle(17)
+            time.sleep(self.OPEN_TERM)
+            self.motor.ChangeDutyCycle(7)
+            time.sleep(self.STOP_TERM)
+            self.motor.ChangeDutyCycle(5)
+            time.sleep(self.CLOSE_TERM)
+
             self.motor.stop()
 
         except KeyboardInterrupt:
@@ -23,4 +30,3 @@ class DoorOperation(threading.Thread):
 
     def setPin(self, p):
         self.motor = p
-        
