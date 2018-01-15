f6b6e165de975756d7a6d528bea05a6a78d5cac1
KeonHee Lee <31424664+KeonHeeLee@users.noreply.github.com>
2018-01-16 03:47:36 +0900

    Manage result messages for sending to server

--- main/RaspberryPi/PiMessage.py
+++ main/RaspberryPi/PiMessage.py
@@ -2,8 +2,7 @@ class PiMessage :
     #Success
     successWaterMessage = "펫에게 물을 줬어요!"
     successFeedMessage = "펫에게 먹이를 줬어요!"
-    successDoorMessage = "펫 하우스를 열어줬어요!"
-    successCameraMessage = "사진을 찍었어요.\n\n"
+    successCameraMessage = "사진을 찍었어요.\n"
 
     def getSuccessWaterMessage(self):
         return self.successWaterMessage
@@ -11,12 +10,8 @@ class PiMessage :
     def getSuccessFeedMessage(self):
         return self.successFeedMessage
 
-    def getSuccessDoorMessage(self):
-        return self.successDoorMessage
-
-    def getSuccessCameraMessage(self, imageURL):
-        successCameraResultMessage = self.successCameraMessage + imageURL
-        return successCameraResultMessage
+    def getSuccessCameraMessage(self):
+        return self.successCameraMessage
 
     #Fail Dict
     failWaterMessage = "펫에게 정상적으로 물을 주지 못했어요. 센서 확인 부탁드릴게요."
@@ -39,7 +34,7 @@ class PiMessage :
     #Continue Dict
     continueWaterMessage = "펫에게 물을 주는 중이예요. 잠시 후 시도해주세요!"
     continueFeedMessage = "펫에게 먹이를 주는 중이예요.  잠시 후 시도해주세요!"
-    continueDoorMessage = "펫 하우스를 열어주는 중이예요. 잠시 후 시도해주세요!"
+    continueDoorMessage = "펫 하우스를 열어주는 중이예요."
     continueCameraMessage = "사진을 찍고있어요. 잠시 후 시도해주세요!"
 
     def getContinueWaterMessage(self):
