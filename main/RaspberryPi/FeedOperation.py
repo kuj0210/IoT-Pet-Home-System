5ee1afda3790e4f501e083620a2c6082ca55854f
KeonHee Lee <31424664+KeonHeeLee@users.noreply.github.com>
2018-01-15 20:23:46 +0900

    Update FeedOperation.py

--- main/RaspberryPi/FeedOperation.py
+++ main/RaspberryPi/FeedOperation.py
@@ -5,16 +5,16 @@ import threading
 class FeedOperation(threading.Thread):
     def __init__(self):
         self.motor = 0
-        self.OPEN_TERM = 1
-        self.CLOSE_TERM = 0.5
+        self.OPEN_TERM = 0.4
+        self.CLOSE_TERM = 1
 
 
     def run(self):
         self.motor.start(0)
         try:
-            self.motor.ChangeDutyCycle(6)
-            time.sleep(self.OPEN_TERM)
             self.motor.ChangeDutyCycle(3)
+            time.sleep(self.OPEN_TERM)
+            self.motor.ChangeDutyCycle(6)
             time.sleep(self.CLOSE_TERM)
             self.motor.stop()
         except KeyboardInterrupt:
@@ -25,4 +25,3 @@ class FeedOperation(threading.Thread):
 
     def setPin(self, p):
         self.motor = p
-        
