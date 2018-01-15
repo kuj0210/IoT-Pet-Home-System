774adbd1e4db5345d2fcedfc2156fb398c3bb7fa
KeonHee Lee <31424664+KeonHeeLee@users.noreply.github.com>
2018-01-15 17:50:36 +0900

    RaspberryPi's main thread

--- main/PiServer.py
+++ main/PiServer.py
@@ -165,3 +165,4 @@ if __name__ == "__main__":
         app.run(host='0.0.0.0', port=8888, debug = True)
     else:
         print("RaspberryPi connection with server is denied or disconnected")
+        
