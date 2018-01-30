ac52211d02a8e80f1981470012c22e111248c05a
KeonHee Lee <31424664+KeonHeeLee@users.noreply.github.com>
2018-01-30 21:13:19 +0900

    v1.1 threading Pi-modules

--- main/RaspberryPi/WaterOperation.py
+++ main/RaspberryPi/WaterOperation.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 import threading
 
@@ -9,22 +9,34 @@ class WaterOperation(threading.Thread):
         self.STOP_TERM = 3
         self.CLOSE_TERM = 0.2
         threading.Thread.__init__(self)
+        self.waterEvent = threading.Event()
 
     def run(self):
-        self.p.start(0)
-        try:
-            self.motor.ChangeDutyCycle(8)
-            time.sleep(self.OPEN_TERM)
-            self.motor.ChangeDutyCycle(6.8)
-            time.sleep(self.STOP_TERM)
-            self.motor.ChangeDutyCycle(5)
-            time.sleep(self.CLOSE_TERM)
-            self.motor.stop()
-        except KeyboardInterrupt:
-            self.motor.stop()
-            return "fail", False
+        #self.p.start(0)
+        while not self.waterEvent.isSet():
+            try:
+                self.waterEvent.wait()
+                print("Water 시작!")
+                #self.motor.ChangeDutyCycle(8)
+                time.sleep(self.OPEN_TERM)
+                #self.motor.ChangeDutyCycle(6.8)
+                time.sleep(self.STOP_TERM)
+                #self.motor.ChangeDutyCycle(5)
+                time.sleep(self.CLOSE_TERM)
+                #self.motor.stop()
+                print("Water 완료")
+                self.waterEvent.clear()
 
-        return "success", False
+            except KeyboardInterrupt:
+                print("Water operation interrupt")
+                self.waterEvent.clear()
+                #self.motor.stop()
 
     def setPin(self, p):
         self.motor = p
+
+    def isWaterOnUnlock(self):
+        return self.waterEvent.isSet()
+
+    def setWaterOperation(self):
+        self.waterEvent.set()
