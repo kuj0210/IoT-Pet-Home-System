1f947cb1701a85080c376ea8a95adfd04ad5271b
KeonHee Lee <31424664+KeonHeeLee@users.noreply.github.com>
2018-01-30 21:14:41 +0900

    v1.1 threading Pi-modules

--- main/RaspberryPi/DoorOperation.py
+++ main/RaspberryPi/DoorOperation.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 import threading
 
@@ -9,22 +9,34 @@ class DoorOperation(threading.Thread):
         self.STOP_TERM = 3
         self.CLOSE_TERM = 4
         threading.Thread.__init__(self)
+        self.doorEvent = threading.Event()
 
     def run(self):
-        self.motor.start(0)
-        try:
-            self.motor.ChangeDutyCycle(17)
-            time.sleep(self.OPEN_TERM)
-            self.motor.ChangeDutyCycle(7)
-            time.sleep(self.STOP_TERM)
-            self.motor.ChangeDutyCycle(5)
-            time.sleep(self.CLOSE_TERM)
+        #self.motor.start(0)
+        while not self.doorEvent.isSet():
+            try:
+                self.doorEvent.wait()
+                print("Door 시작!")
+                #self.motor.ChangeDutyCycle(17)
+                time.sleep(self.OPEN_TERM)
+                #self.motor.ChangeDutyCycle(7)
+                time.sleep(self.STOP_TERM)
+                #self.motor.ChangeDutyCycle(5)
+                time.sleep(self.CLOSE_TERM)
+                #self.motor.stop()
+                print("Door 완료")
+                self.doorEvent.clear()
 
-            self.motor.stop()
-
-        except KeyboardInterrupt:
-            print("Door operation interrupt")
-            self.motor.stop()
+            except KeyboardInterrupt:
+                print("Door operation interrupt")
+                self.doorEvent.clear()
+                #self.motor.stop()
 
     def setPin(self, p):
         self.motor = p
+
+    def isDoorOnUnlock(self):
+        return self.doorEvent.isSet()
+
+    def setDoorOperation(self):
+        self.doorEvent.set()
