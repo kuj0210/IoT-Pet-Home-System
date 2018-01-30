4929458002a3634bf4e137cec9004e9cd2c65bfe
KeonHee Lee <31424664+KeonHeeLee@users.noreply.github.com>
2018-01-30 21:15:15 +0900

    v1.1 threading Pi-modules

--- main/RaspberryPi/CameraOperation.py
+++ main/RaspberryPi/CameraOperation.py
@@ -1,4 +1,4 @@
-import picamera
+#import picamera
 import time
 import threading
 import os
@@ -7,28 +7,34 @@ class CameraOperation(threading.Thread):
     def __init__(self):
         self.filename = "pet_Image.jpg"
         self.path = ""
+        threading.Thread.__init__(self)
+        self.cameraEvent = threading.Event()
+        self.result = False
 
     def run(self):
         dir = "image/"
         if not os.path.isdir(dir):
             os.mkdir(dir)
 
-        try:
-            now = time.localtime()
-            self.filename = "Screenshot (%04d-%02d-%02d %02d:%02d:%02d).png"  \
-                            %(now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+        if not self.cameraEvent.isSet():
+            try:
+                self.cameraEvent.wait()
+                now = time.localtime()
+                self.filename = "Screenshot (%04d-%02d-%02d %02d:%02d:%02d).png"  \
+                                %(now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
 
-            self.path = dir + self.filename
+                self.path = dir + self.filename
 
-            camera.start_preview()
-            time.sleep(1)
-            camera.capture(self.path)
-            camera.stop_preview()
-        except:
-            print("%s is not in this directory or Camera Module error." %(self.path))
-            return "fail", False
-
-        return "success", False
+                #camera.start_preview()
+                time.sleep(1)
+                #camera.capture(self.path)
+                #camera.stop_preview()
+                self.cameraEvent.clear()
+                return "success"
+            except:
+                print("%s is not in this directory or Camera Module error." %(self.path))
+                self.cameraEvent.clear()
+                return "fail"
 
     def getFilename(self):
         return self.filename
@@ -41,3 +47,12 @@ class CameraOperation(threading.Thread):
             os.remove(self.path)
         except:
             print("%s : is not directory or in this directory." %(self.path))
+
+    def getResult(self):
+        return self.result
+
+    def isCameraOnUnlock(self):
+        return self.cameraEvent.isSet()
+
+    def setCameraOperation(self):
+        self.cameraEvent.set()
