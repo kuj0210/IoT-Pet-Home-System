84d81aab130c720cceae9b2271ba2783da0aa3c6
K.H Lee <beta1360@naver.com>
2018-03-09 22:11:10 +0900

    fix bug

--- main/compare.py
+++ main/compare.py
@@ -6,6 +6,7 @@ LICENSE : GPL v3 LICENSE
 - Description : https://github.com/kuj0210/IoT-Pet-Home-System
 - If you want to contact us, please send mail "beta1360@naver.com"
 '''
+import time
 
 from konlpy.tag import Kkma
 
@@ -19,17 +20,24 @@ class KeyWord:
         self.n=[]
         self.v=[]
         self.gravity=0
+        self.andList=['와','과','고','이랑']
+
     def setNouns(self,list):
         self.n=list
+
     def setVerbs(self,list):
         self.v=list
+
     def setGravity(self,gravity):
         self.gravity=gravity
+
     def isMe(self,sentence,originLIST):
         count =0
         i=0
-        index = -1
-
+        flag = -1
+        index =-1
+        AND = "NODATA"
+        #print(sentence)
         for noun in self.n:
             for i in range(len(sentence)):
                 if noun == sentence[i]:
@@ -38,6 +46,7 @@ class KeyWord:
                     break
             if count>=GRAVITY_N:
                 break
+
         first = count
         for verb in self.v:
             for j in range(i+1,len(sentence)):
@@ -48,31 +57,24 @@ class KeyWord:
             if count>first:
                 break
 
-        flag = -1
-        if '고' in sentence:
-            index = sentence.index('고')
-            flag = 1
-        elif '이랑' in sentence:
-            index = sentence.index('이랑')
-            flag = 2
+        for AND in self.andList:
+            if AND in sentence:
+                index = sentence.index(AND)
+                break
+
         if index !=-1 and 'N' in originLIST[index-1][1]and 'N' in originLIST[index+1][1]:
-            flag*=10
-
-
+            #print("TRUE")
+            flag=True
 
         if self.gravity<=count:
             if noun in sentence:
                 sentence.remove(noun)
-            if verb in sentence and flag <10:
+            if verb in sentence and flag ==-1:
                 sentence.remove(verb)
-
-            if flag == 10:
-                sentence.remove("고")
-            elif flag == 20:
-                sentence.remove("이랑")
+            if flag == True and index!=-1:
+                sentence.remove(AND)
 
             return True
-
 
         return False
     def _print(self):
@@ -89,17 +91,19 @@ class UsecaseList:
         self.usecase=[]
         self.parsingNVLIST =[]
         self.parsingLIST = []
+
     def getNV(self,sentence):
         self.parsingNVLIST=[]
         self.parsingLIST=[]
         data =self.kkma.pos(sentence)
+        #print(data)
+
         for list in data:
-            if 'N' in list[1] or list[1].count('V')>1 or'C' in list[1] :
+            if 'N' in list[1] or list[1].count('V')>1 or'C' in list[1]or'JKM' in list[1] :
                 self.parsingNVLIST.append(list[0])
                 self.parsingLIST.append(list)
-        #print(NVList)
+        print(self.parsingNVLIST)
     ##don't Touch
-
 
     def printList(self):
         for item in self.usecase:
@@ -114,6 +118,7 @@ class UsecaseList:
                 request.append(item.keyWord)
         print(request)
         return request
+
     #it can set your UseCase
     def setUsecase(self, name, nList, vList, gravity):
         self.usecase.append(KeyWord(name))
