0c809afd905b3e05e975727af81fe3a2a66b1f89
K.H Lee <beta1360@naver.com>
2018-03-14 16:40:51 +0900

    add remarking code

--- main/Server.py
+++ main/Server.py
@@ -4,7 +4,12 @@ LICENSE : GPL v3 LICENSE
 
 - Description : https://github.com/kuj0210/IoT-Pet-Home-System
 - If you want to contact us, please send mail "beta1360@naver.com"
+
+* Server.py
+  - Core Modules That Work with Server.
+  - Perform the role of the main server by running this module
 '''
+
 #-*-coding: utf-8-*-
 
 from ServerManagerForKakao import KakaoMessageClass
@@ -33,15 +38,29 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/",methods=["POST"])
 def naver_ServerManager():
+'''
+- Related Naver-talk-talk Platform
+- This function manage the related Naver-talk-talk API 
+  and reply to Naver-talk-talk API server.  
+'''
     data = request.get_json()
     return jsonify(naverMessage.manageEvent(data=data, usecase=usecase)),200
 
 @app.route("/keyboard",methods =["GET"])
 def kakao_Keyboard():
+'''
+- Related Kakao-talk Platform
+- This function initialize and call kakao_talk's keyboard.
+'''
     return jsonify(kakaoMessage.getBaseKeyboard()),200
 
 @app.route("/message",methods=["POST"])
 def kakao_Message():
+'''
+- Related Kakao-talk Platform
+- This function manage message(text) from Kakao-platform client
+  and reply a appropriate message to the client.
+'''
     data = request.get_json()
     user_key = data['user_key']
     message = data['content']
@@ -49,18 +68,38 @@ def kakao_Message():
 
 @app.route("/friend",methods=["POST"])
 def kakao_AddFriend():
+'''
+- Related Kakao-talk Platform
+- This function manage request about adding friend.
+- Send only 200 OK responses, as there is nothing special to manage with the request.
+'''
     return "HTTP/1.1 200 OK"
 
 @app.route("/friend/<user_key>", methods=["DELETE"])
 def kakao_DeleteFriend(user_key):
+'''
+- Related Kakao-talk Platform
+- This function manage request about deleting friend.
+- Send only 200 OK responses, as there is nothing special to manage with the request.
+'''
     return "HTTP/1.1 200 OK"
 
 @app.route("/chat_room/<user_key>",methods=["DELETE"])
 def kakao_chat_roomOut():
+'''
+- Related Kakao-talk Platform
+- This function manage request about joining chatting room.
+- Send only 200 OK responses, as there is nothing special to manage with the request.
+'''
     return "HTTP/1.1 200 OK"
 
 @app.route("/pi_regist", methods=["POST"])
 def settingPi():
+'''
+- Related to control Raspberry-Pi
+- This function initialize the related device.
+- When the device is turned on, it is a function of receiving and processing a request.
+'''
     data = request.get_json()
     PiKey = data["PiKey"]
     kakaoUserList = data["userList"]["kakao"]
@@ -71,10 +110,18 @@ def settingPi():
 
 @app.route('/download/<filename>', methods=['GET'])
 def send_file(filename):
+'''
+- Only related Kakao-talk Platform
+- This function send the image file:<filename> to client who request this image file.
+'''
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename=filename)
 
 @app.route('/push', methods=["POST"])
 def push_alarm():
+'''
+- Only related Naver-talk-talk Platform
+- This function is intended to provide alarm services to the user.
+'''
     data = request.get_json()
     PiKey = data["PiKey"]
     message = data["message"]
@@ -82,6 +129,10 @@ def push_alarm():
     return "HTTPS/1.1 200 OK"
 
 if __name__ == "__main__":
+'''
+It's the main part. After you set up the SSL certificate path, 
+attach it to the flask framework and launch the Web server.
+'''
     ssl_cert = '/etc/letsencrypt/live/pethome.ga/fullchain.pem'
     ssl_key =  '/etc/letsencrypt/live/pethome.ga/privkey.pem'
     contextSSL =  (ssl_cert, ssl_key)
