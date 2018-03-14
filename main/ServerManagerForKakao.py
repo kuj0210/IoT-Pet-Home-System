422677fab9ac3d1bd5c02a1f29ea88ca2c09a2d0
K.H Lee <beta1360@naver.com>
2018-03-14 20:33:10 +0900

    Update ServerManagerForKakao.py

--- main/ServerManagerForKakao.py
+++ main/ServerManagerForKakao.py
@@ -4,25 +4,27 @@ LICENSE : GPL v3 LICENSE
 
 - Description : https://github.com/kuj0210/IoT-Pet-Home-System
 - If you want to contact us, please send mail "beta1360@naver.com"
+
+* ServerManagerForKakao.py
+  - This module is used to process data from the Kakao-talk platform.
+  -  This module uses to Server module for managing json data.
+     Specially, This module manage json data that use to send or recieve kakao server.
+     ** Json data usually use HTTP(S) Protocol's body part.
 '''
 # -*-coding: utf-8-*-
+
 from ServerUtility import ServerUtility
 from ResponseMessage import Message
-
-#
-# This module uses to Server module for managing json data.
-# Specially, This module manage json data that use to send or recieve kakao server.
-# ** Json data usually use HTTP(S) Protocol's body part.
-#
-
-# 동사, 명사중 아무거나 만족 : 50
-# 명사 반드시 만족 : 60
-# 모두 만족: 100
 
 mServerUtility = ServerUtility()
 responseMessage = Message()
 
 class KakaoMessageClass :
+    '''
+    - This class manage kakao-talk platform operations.
+    - For more complex operations, check the "ServerUtility.py" module.
+    '''
+    
     def __init__(self):
         self.sendMSG = "None"
         self.NOT_REGISTED = "등록되지 않은 유저입니다. 사용자 등록부터 진행해주세요."
@@ -30,15 +32,24 @@ class KakaoMessageClass :
         self.KAKAO_PLATFORM = "kakao-talk"
 
     def getBaseKeyboard(self):
+        # This function initialize base_kakao_Keyboard_reply.
         baseKeyboard = {
             "type": "text"
         }
         return baseKeyboard
 
     def manageRequest(self, user_key, message, usecase):
+        '''
+        This function is performed in three steps.
+        - step 1. To analyze natual sentence (from client) and seperate it down into commands to be executed on the device.
+        - step 2. To send the seperated commands to device and recieve reply from the device.
+        - step 3. To reply the appropriate message to client.
+        '''
         result = usecase.analyzeSentence(message)
         print(result)
 
+        # ------ Step 1 end ------
+        
         if 'regist' in result:
             self.sendMSG = \
                 responseMessage.regist_userMessage(message=message,user_key=user_key,mServerUtility=mServerUtility)
@@ -70,6 +81,8 @@ class KakaoMessageClass :
 
             else: # elif isRegistedUser is False
                 self.sendMSG = self.NOT_REGISTED
+                
+           # ------ Step 2 end ------
 
             postBodyMessage = {
                 'message': {
@@ -78,3 +91,5 @@ class KakaoMessageClass :
                 'keyboard': self.getBaseKeyboard()
             }
             return postBodyMessage
+
+            # ------ Step 3 end ------
